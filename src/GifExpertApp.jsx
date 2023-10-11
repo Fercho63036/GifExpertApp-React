f0172435b2b2d0e0f9cd571194310094b82f0fcc
Ariel Paricagua <ArielFParicaguaV@gmail.com>
2023-10-11 15:58:41 -0400

    Agregando componentes y lista de categorias

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Naruto' ]);
+
+    console.log(categories);
+
+    return (
+        <>
+            {/* Titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            <ol>
+                { categories.map( category => {
+                        return <li key={ category } >{ category }</li>
+                    })
+                }
+                {/* <li>ABC</li> */}
+            </ol>
+                {/* Gif Item */}
+
+        </>
+    )
+}
