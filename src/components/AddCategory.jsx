d9e99d3600a7438005d647b6150d85a4083c2861
Ariel Paricagua <ArielFParicaguaV@gmail.com>
2023-10-18 10:48:23 -0400

    Archivos de barril

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const AddCategory = ({ onNewCategory }) => {
+export const AddCategory = ({ onNewCategory }) => {
   
     const [ inputValue, setInputValue ] =  useState();
 
@@ -31,5 +31,3 @@ const AddCategory = ({ onNewCategory }) => {
         </form>
   )
 }
-
-export default AddCategory;
